5516decafb3dff94577e0ed3eb7b6cb7ffdebef1
Tim Nilsson <tim.nilsson97@hotmail.com>
2022-11-22 10:10:33 +0100

    Add Customer

---
+++ BankSystem/AddCustomer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    internal class AddCustomer
+    {
+    }
+}
--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -6,7 +6,48 @@ namespace BankSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer C1 = new Customer()
+            {
+                ID = 1,
+                Name = "Tim",
+                UserName = "Tim1",
+                Password = "1234",
+                Currency = 3000
+
+            };
+
+            Customer C2 = new Customer()
+            {
+                ID = 2,
+                Name = "Elin",
+                UserName = "Elin1",
+                Password = "4321",
+                Currency = 4000
+
+            };
+
+            Customer C3 = new Customer()
+            {
+                ID = 3,
+                Name = "Dennis",
+                UserName = "Dennis1",
+                Password = "5678",
+                Currency = 5000
+
+            };
+
+            Customer C4 = new Customer()
+            {
+                ID = 4,
+                Name = "Anton",
+                UserName = "Anton1",
+                Password = "8765",
+                Currency = 6000
+
+            };
+
+           
+
         }
     }
 }
