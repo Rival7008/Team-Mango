6d05926f5b89ccb4c53fd6ceb25a63045b7d83a4
Tim Nilsson <tim.nilsson97@hotmail.com>
2022-11-22 12:05:50 +0100

    Test

--- BankSystem/User.cs
+++ BankSystem/User.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace BankSystem
 {
-    internal class Customers
+    internal class User
     {
+        private int _id;
+        public string Name;
+        private string _username;
+        private string _password;
+        public decimal _currency;
+        public int ID { get { return _id; } set { _id = value; } }
+        public string UserName { get { return _username; } set { _username = value; } }
+        public string Password { get { return _password; } set { _password = value; } }
+        public decimal Currency { get { return _currency; } set { _currency = value; } }
+
+
+
+
     }
 }
